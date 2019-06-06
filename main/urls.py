54ac1fa32a6dccd2ef4d2e719f41d7c80e0b8a42
Chinmay Hebbar <4chinmai@gmail.com>
2019-06-06 19:57:54 +0530

    wip bill entering support

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from .views import PatientList, Login, LogoutReq
+from .views import PatientList, Login, LogoutReq, AddItem
 
 urlpatterns = [
     # url(r'patients', PatientListView.as_view(), name='listpatients'),
     url(r'^patientlist/', PatientList, name='listofpatients'),
     url(r'^login/?', Login, name='login'),
     url(r'^logout/?', LogoutReq, name='logout'),
+    url(r'^add_item/?', AddItem, name='add_item'),
 ]
